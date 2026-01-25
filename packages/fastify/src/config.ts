b41e2b63c12fe3330596ca37f3ac9f07d3c66781
Ericgi231 <ericgi231@gmail.com>
2026-01-25 17:16:15 -0600

    Initial commit

--- packages/fastify/src/config.ts
+++
@@ -1,4 +0,0 @@
-const env = (process.env.NODE_ENV || 'production').toLowerCase();
-const isDev = env === 'development';
-const port = Number(process.env.DEV_PORT ?? 3000);
-export default { env, isDev, port };

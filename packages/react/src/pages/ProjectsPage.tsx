0f889e77256007f3b0926b9c8f3fd3061a1b896c
Ericgi231 <ericgi231@gmail.com>
2026-01-26 20:48:45 -0600

    Added icon + warning that the projects are being ported

--- packages/react/src/pages/ProjectsPage.tsx
+++ packages/react/src/pages/ProjectsPage.tsx
@@ -4,6 +4,7 @@ const ProjectsPage = () => {
   return (
     <>
       <NameAndLinks />
+      <p className="flex content-center justify-center text-lg text-text">NOTE: Projects are being ported to new containerized infrastructure, please check back later.</p>
       <ProjectGrid />
     </>
   )

0b5c6b2beb171ff1080eb0c21ca6304c395ab544
Ericgi231 <ericgi231@gmail.com>
2026-02-06 14:30:30 -0600

    Adding a note about server downtime

--- packages/react/src/pages/ProjectsPage.tsx
+++ packages/react/src/pages/ProjectsPage.tsx
@@ -5,6 +5,7 @@ const ProjectsPage = () => {
     <>
       <NameAndLinks />
       <p className="flex content-center justify-center text-lg text-text">NOTE: Projects are being ported to new containerized infrastructure, please check back later.</p>
+      <p className="flex content-center justify-center text-lg text-text">NOTE: Currently moving houses, expect prolonged downtime.</p>
       <ProjectGrid />
     </>
   )
